ecb93f927a256cf1782917ce116fd6b87646882f
Igor Kulman <igor@kulman.sk>
2015-03-27 11:06:15 +0100

    * Nuget package version incremented

--- build.fsx
+++ build.fsx
@@ -9,7 +9,7 @@ let buildDir = @".\build\"
 let testDir  = @".\test\"
 let packagesDir = @".\packages"
 let packagingRoot = "./packaging/"
-let packagesVersion = "1.0.3"
+let packagesVersion = "1.0.4"
 
 // Targets
 Target "Clean" (fun _ ->
